133609d51a0b951973aecf057d594f6635398679
Carlos Galindo <utm22030587@utma.edu.mx>
2024-07-31 20:52:45 -0600

    feat: add environment variables

--- axios/axios.tsx
+++ axios/axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: "https://api-reactnative.onrender.com/api/products",
+    baseURL: process.env.EXPO_PUBLIC_API_URL,
     headers: {
         "Content-Type": "application/json",
     },
